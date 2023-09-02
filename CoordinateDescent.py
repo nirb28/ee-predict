ac6f0fa4b480bdf116a2a358d74d1b1fa6150205
nirbaan <nirbaanm@gmail.com>
2023-09-02 16:48:11 -0400

    CD 1st attempt

--- CoordinateDescent.py
+++ CoordinateDescent.py
@@ -60,3 +60,52 @@ plt.plot(range(len(xret)), xret, label="GD")
 plt.plot(range(len(cxret)), cxret, label="CD")
 plt.legend()
 plt.show()
+
+# Nirbaan
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+
+# Sample data: Replace with your actual data
+catalyst_data = np.random.rand(300, 3)  # Catalyst properties (x, y, z)
+original_ee = np.random.rand(300)# Original enantiomeric excess values
+
+# Function to optimize catalyst properties using coordinate descent
+def optimize_catalysts(catalysts, ee_values, iterations=100, step_size=0.01):
+    optimized_catalysts = np.copy(catalysts)
+    for _ in range(iterations):
+        for i in range(len(optimized_catalysts)):
+            original_ee = predict_ee(optimized_catalysts[i])  # Use your regression model
+            for x in range(len(optimized_catalysts[i])):
+                old_value = optimized_catalysts[i, x]
+                optimized_catalysts[i, x] = old_value + step_size
+                new_ee = predict_ee(optimized_catalysts[i])
+                if new_ee < original_ee:
+                    optimized_catalysts[i, x] = old_value - step_size
+                    new_ee = predict_ee(optimized_catalysts[i])
+                if new_ee < original_ee:
+                    optimized_catalysts[i, x] = old_value
+    return optimized_catalysts
+
+# Function to predict enantiomeric excess using regression model
+def predict_ee(properties):
+    # Use your regression model to predict EE based on properties
+    # Replace this with your actual prediction code
+    return np.random.rand()
+
+# Perform optimization on the first 150 catalysts
+optimized_catalysts = optimize_catalysts(catalyst_data[:150], original_ee[:150])
+
+# Perform k-nearest neighbors analysis
+num_neighbors = 5  # Number of neighbors to consider
+knn = NearestNeighbors(n_neighbors=num_neighbors)
+knn.fit(catalyst_data[150:])  # Using the remaining 150 catalysts for validation
+
+# Validate optimized catalysts using KNN
+for i in range(len(optimized_catalysts)):
+    distances, indices = knn.kneighbors([optimized_catalysts[i]])
+    neighbors_ee = [original_ee[150 + idx] for idx in indices[0]]
+    optimized_ee = predict_ee(optimized_catalysts[i])
+    average_neighbors_ee = np.mean(neighbors_ee)
+    print(f"Optimized EE: {optimized_ee:.4f} vs. Average Neighbors' EE: {average_neighbors_ee:.4f}")
+
+# Further analysis and interpretation can be added here
